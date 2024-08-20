8fc8adae8a36e3b28ec83940d931a134509aec47
Jeremy Pewterschmidt (Shixin Wang) <jeremypewterschmidt@gmail.com>
2024-08-20 15:45:37 +0800

    add insert sort and bobble sort

--- src/sort.h
+++ src/sort.h
@@ -25,6 +25,40 @@ void quick_sort(::std::forward_iterator auto first, ::std::forward_iterator auto
     quick_sort(middle2, last);
 }
 
+void bobble_sort(::std::forward_iterator auto first, ::std::forward_iterator auto last)
+{
+    if (first == last) 
+        return;
+
+    for (auto pivot = first; pivot != last; ::std::advance(pivot, 1))
+    {
+        for (auto i = ::std::next(pivot); i != last; ::std::advance(i, 1))
+        {
+            if (::std::less{}(*i, *pivot))
+                ::std::swap(*i, *pivot);
+        }
+    }
+}
+
+void insert_sort(::std::random_access_iterator auto first, ::std::random_access_iterator auto last)
+{
+    if (first == last) 
+        return;
+
+    for (auto it = first + 1; it != last; ++it) 
+    {
+        auto key = *it;
+        auto j = it - 1;
+
+        while (j >= first && key < *j) 
+        {
+            *(j + 1) = *j;
+            --j;
+        }
+        *(j + 1) = key;
+    }
+}
+
 END_NAMESPACE_DSNUGEE
 
 #endif
--- tests/sort.cc
+++ tests/sort.cc
@@ -2,13 +2,38 @@
 #include "gtest/gtest.h"
 
 #include <vector>
+#include "fmt/ranges.h"
+#include "fmt/format.h"
 
 TEST(sort, quick_sort)
 {
-    ::std::vector v1{ 5,6,7,8,4,32,44,5,6,8,8,99 };
+    ::std::vector v1{ 5,6,7,8,4,32,44,5,6,8,8,99,0 };
     ::std::vector v2 = v1;
     ::std::sort(v1.begin(), v1.end());
+
     dsnugee::quick_sort(v2.begin(), v2.end());
-    
-    ASSERT_EQ(v1, v2);
+
+    ASSERT_EQ(v1, v2) << fmt::format("{}", v2);
+}
+
+TEST(sort, bobble_sort)
+{
+    ::std::vector v1{ 5,6,7,8,4,32,44,5,6,8,8,99,0 };
+    ::std::vector v2 = v1;
+    ::std::sort(v1.begin(), v1.end());
+
+    dsnugee::bobble_sort(v2.begin(), v2.end());
+
+    ASSERT_EQ(v1, v2) << fmt::format("{}", v2);
+}
+
+TEST(sort, insert_sort)
+{
+    ::std::vector v1{ 5,6,7,8,4,32,44,5,6,8,8,99,0 };
+    ::std::vector v2 = v1;
+    ::std::sort(v1.begin(), v1.end());
+
+    dsnugee::insert_sort(v2.begin(), v2.end());
+
+    ASSERT_EQ(v1, v2) << fmt::format("{}", v2);
 }
